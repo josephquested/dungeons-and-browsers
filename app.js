987d7c2b596e6e61e184c3875daf42ced513c982
josephquested <josephquested@gmail.com>
2016-12-08 13:09:37 +1300

    initial working auth with dummy db

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
+// --- auth --- //
+var passport = require('passport')
+var Strategy = require('passport-local').Strategy
+
+var db = require('./db/db')
+
+passport.use(new Strategy((username, password, cb) => {
+  db.users.findByUsername(username, (err, user) => {
+    if (err) return cb(err)
+    if (!user) return cb(null, false)
+    if (user.password != password) return cb(null, false)
+    return cb(null, user)
+  })
+}))
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+
+passport.deserializeUser((id, cb) => {
+  db.users.findById(id, (err, user) => {
+    if (err) return cb(err)
+    cb(null, user)
+  })
+})
+
+// --- express --- ///
 var express = require('express')
 var templit = require('templit')
 
 var app = express()
-
 app.engine('js', templit)
 app.set('view engine', 'js')
-
-app.use(express.static(`${__dirname}/public`))
 app.set('views', `${__dirname}/views`)
 
+app.use(express.static(`${__dirname}/public`))
+app.use(require('morgan')('combined'))
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// --- routes --- //
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home', { user: req.user })
 })
 
 app.get('/play', (req, res) => {
   res.render('play')
 })
 
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/' }), (req, res) => { res.redirect('/') }
+)
+
+
 app.listen(3000, () => {
-  console.log('templit app rendering on port 3000')
+  console.log('DUNGEONS AND BROWSERS / PORT 3000')
 })
--- views/home.js
+++ views/home.js
@@ -1,6 +1,49 @@
 var header = require('./partials/header')
 
 function render (data) {
+  if (!data.user) {
+    return notAuthorized()
+  } else {
+    return authorized()
+  }
+}
+
+function notAuthorized () {
+  return `
+    <div id="header">
+      <span id="navs">
+        <h1>DUNGEONS AND BROWSERS</h1>
+      </span>
+      <hr>
+    </div>
+
+    <div id="login-form">
+      <h2>login</h2>
+      <form action="/login" method="post">
+        <label>Username:</label>
+        <input type="text" name="username"/><br/>
+        <label>Password:</label>
+        <input type="password" name="password"/>
+        <input type="submit" value="->"/>
+      </form>
+    </div>
+
+    <br><br>
+
+    <div id="logup-form">
+      <h2>logup</h2>
+      <form action="/logup" method="post">
+        <label>Username:</label>
+        <input type="text" name="username"/><br/>
+        <label>Password:</label>
+        <input type="password" name="password"/>
+        <input type="submit" value="->"/>
+      </form>
+    </div>
+  `
+}
+
+function authorized (data) {
   return `
     ${header('home')}
   `
