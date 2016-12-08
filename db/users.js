6709fc44a971a7f307f910344b03ee0dc281633a
josephquested <josephquested@gmail.com>
2016-12-08 17:01:35 +1300

    db working

--- db/users.js
+++ db/users.js
@@ -1,32 +1,29 @@
-var records = [
-    { id: 1, username: 'jos', password: 'jos', displayName: 'Jos', emails: [ { value: 'jack@example.com' } ] }
-  , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
-];
-
-function findById (id, cb) {
-  process.nextTick(function() {
-    var idx = id - 1;
-    if (records[idx]) {
-      cb(null, records[idx]);
-    } else {
-      cb(new Error('User ' + id + ' does not exist'));
-    }
-  })
-}
-
-function findByUsername (username, cb) {
-  process.nextTick(function() {
-    for (var i = 0, len = records.length; i < len; i++) {
-      var record = records[i];
-      if (record.username === username) {
-        return cb(null, record);
-      }
-    }
-    return cb(null, null);
-  })
-}
+var env = process.env.NODE_ENV || 'development'
+var config = require('./knexfile')[env]
+var knex = require('knex')(config)
 
 module.exports = {
-  findById,
-  findByUsername
+  findAll: (table, callback) => {
+    knex.raw('select * from users')
+    .then(res => callback(null, res))
+  },
+
+  findByUsername: (username, callback) => {
+    console.log('TRYING TO FIND USER: ', username)
+    knex('users')
+    .where('username', username)
+    .then(res => callback(null, res[0]))
+  },
+
+  findById: (id, callback) => {
+    knex('users')
+    .where('id', id)
+    .then(res => callback(null, res[0]))
+  },
+
+  addUser: (data, callback) => {
+    knex('users')
+    .insert({ username: data.username, password: data.password })
+    .then(res => callback(null, res))
+  }
 }
